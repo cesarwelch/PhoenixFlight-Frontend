ec730372ed8158f5f60fcba056c44152b2777e11
Cesar <cesar777welch@gmail.com>
2018-07-15 17:50:02 -0600

    first commit

---
+++ src/components/test.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="test">
+		<h2>Essential Links</h2>
+	</div>
+</template>
+
+<script>
+export default {
+
+  name: 'test',
+
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
