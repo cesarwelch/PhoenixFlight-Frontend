803f741770fb3a41a6bae828cc3cfe299738e4e5
Cesar <cesar777welch@gmail.com>
2018-07-29 19:59:15 -0600

    change back again

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import home from '@/components/home'
-
 Vue.use(Router)
-
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: home
-    }
-  ]
+  mode: 'history',
+  routes: [{
+    path: '/',
+    name: 'home',
+    component: home
+  }]
 })
