c80cf270f13c4c7b738b8fc766ccbcd39f3cceda
Cesar <cesar777welch@gmail.com>
2018-08-28 11:00:55 -0600

    preview

--- src/components/main-slider.vue
+++ src/components/main-slider.vue
@@ -3,47 +3,77 @@
         <div class="display-table center-text">
             <div class="display-table-cell">
                 <div class="slider-content">
-                    <h5 class="date slider-style-1">
-                        Con el infinito apoyo y la bendición de nuestros Padres
-                    </h5>
                     <div class="container">
-                        <div class="row slider-style-3">
-                            <div class="col-sm"><h4>Edwin Alfredo Fernández Batrez</h4></div>
-                            <div class="col-sm"><h4>Marvelí del Socorro Cano Rodríguez</h4></div>
+                        <div class="row">
+                            <h5 class="slider-style-4">
+                                Con el infinito apoyo y la bendición de nuestros Padres
+                            </h5>
                         </div>
                         <div class="row slider-style-3">
-                            <div class="col-sm"><h4>y</h4></div>
+                            <div class="col-sm">
+                                <h4>
+                                    Edwin Alfredo Fernández Batrez
+                                </h4>
+                            </div>
+                            <div class="col-sm">
+                                <h4>
+                                    Carlos Roberto Sarmiento Cubero
+                                </h4>
+                            </div>
                         </div>
                         <div class="row slider-style-3">
-                            <div class="col-sm"><h4>Claudia Patricia Sabillón Paz</h4></div>
-                            <div class="col-sm"><h4>Carlos Roberto Sarmiento Cubero</h4></div>
+                            <div class="col-sm">
+                                <h4>
+                                    y
+                                </h4>
+                            </div>
                         </div>
+                        <div class="row slider-style-3">
+                            <div class="col-sm">
+                                <h4>
+                                    Claudia Patricia Sabillón Paz
+                                </h4>
+                            </div>
+                            <div class="col-sm">
+                                <h4>
+                                    Marvelí del Socorro Cano Rodríguez
+                                </h4>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <h4 class="pre-title slider-style-1">
+                                Nosotros
+                            </h4>
+                        </div>
+                        <div class="row">
+                            <h1 class="title slider-style-2">
+                                Franklyn Alfredo Fernández Sabillón
+                            </h1>
+                        </div>
+                        <div class="row">
+                            <h2 class="slider-style-2">
+                                y
+                            </h2>
+                        </div>
+                        <div class="row">
+                            <h1 class="title slider-style-2">
+                                María José Rivera Cano
+                            </h1>
+                        </div>
+                        <h4 class="slider-style-3">
+                            Quisiéramos que nos honren con su presencia el día de nuestra boda
+                        </h4>
                     </div>
-                    <h4 class="pre-title slider-style-1">
-                    Nosotros
-                    </h4>
-                    <h1 class="title slider-style-2">
-                        Franklyn Alfredo Fernández Sabillón
-                    </h1>
-                    <h2 class="slider-style-2">
-                        y
-                    </h2>
-                    <h1 class="title slider-style-2">
-                        María José Rivera Cano
-                    </h1>
-                    <h3 class="date">
-                        Quisiéramos que nos honren con su presencia el día de nuestra boda
-                    </h3>
                 </div>
-                <!-- slider-content-->
             </div>
-            <!--display-table-cell-->
+            <!-- slider-content-->
         </div>
-        <!-- display-table-->
+        <!--display-table-cell-->
     </div>
+    <!-- display-table-->
 </template>
 <script>
-export default {
+    export default {
 
   name: 'mainSlider',
 
