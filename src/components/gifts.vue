85718636dcf3336079dfb4c65ab0b39bf8e6c674
Cesar <cesar777welch@gmail.com>
2018-10-18 16:05:56 -0600

    fix

--- src/components/gifts.vue
+++ src/components/gifts.vue
@@ -25,16 +25,16 @@
                             </div>
                         </div>
                         <div class="row">
-                            <div  class="col-sm ceremony-text-space">
-                                <button @mouseleave="hideAccount1" @mouseover="viewAccount1" class="btn btn-circle btn-xl" type="button">
+                            <div @mouseleave="hideAccount" class="col-sm ceremony-text-space">
+                                <button :class="{expand:account1.selected}" @mouseover="viewAccount1" class="btn btn-circle btn-xl" type="button">
                                     <img v-if="flag1" alt="Gallery Image" class="image-bac-icon" src="../../src/assets/images/icons/bac3.png"/>
                                     <h5 class="cuentas" v-if="!flag1">María José</h5>
                                     <h5 class="cuentas" v-if="!flag1">Rivera Cano</h5>
                                     <h5 class="cuentas" v-if="!flag1">$ 740973711</h5>
                                 </button>
                             </div>
-                            <div  class="col-sm">
-                                <button @mouseleave="hideAccount2" @mouseover="viewAccount2" class="btn btn-circle btn-xl" type="button">
+                            <div @mouseleave="hideAccount" class="col-sm">
+                                <button :class="{expand:account2.selected}"  @mouseover="viewAccount2" class="btn btn-circle btn-xl" type="button">
                                     <img v-if="flag2" alt="Gallery Image" class="image-bac-icon" src="../../src/assets/images/icons/bac3.png"/>
                                     <h5 class="cuentas" v-if="!flag2">Franklyn Alfredo</h5>
                                     <h5 class="cuentas" v-if="!flag2">Fernández Sabillón</h5>
@@ -65,21 +65,25 @@ export default {
   data () {
     return {
       flag1: flag,
-      flag2: flag
+      flag2: flag,
+      account1: {},
+      account2: {}
     }
   },
   methods: {
     viewAccount1: function (event) {
       this.flag1 = 0
+      this.account1 = { selected: true }
     },
     viewAccount2: function (event) {
       this.flag2 = 0
+      this.account2 = { selected: true }
     },
-    hideAccount1: function (event) {
+    hideAccount: function (event) {
       this.flag1 = 1
-    },
-    hideAccount2: function (event) {
+      this.account1.selected = false
       this.flag2 = 1
+      this.account2.selected = false
     }
   }
 }
